c2c58f4aca48b53d6efbe94da971a53c9508622e
Denis Malyshev <dmalyshev.spb@gmail.com>
2024-09-16 04:25:45 +0400

    used HTML POS templates

--- python/deviceEmulator.py
+++ python/deviceEmulator.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Depends, Request, WebSocket, WebSocketException, Query, Cookie, status
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -9,7 +9,6 @@ from pydantic import BaseModel
 from TransLink import Event, Events
 from pprint import pprint
 
-#1from .deviceEmulator import createevent_CLOSEDAY_ONPRINT
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -21,13 +20,44 @@ licenseToken = "e034d5a6cf3212826c57f35cffb103905afe5936/f86419d06688b6336ddfe68
 accessToken = "7C65891202494794599402621701736360258001"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=accessToken)
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    #return {"Hello": "TransLink POS emulator - see docs http://127.0.0.1:6678/docs#/"}
-    id = "111222333"
+    '''This is a start page'''
+    myid = "111222333"
     return templates.TemplateResponse(
-        request=request, name="start.html", context={"id": id}
+        request=request, name="start.html", context={"id": myid}
     )
+
+
+async def get_cookie_or_token(
+    websocket: WebSocket,
+    session: Annotated[str | None, Cookie()] = None,
+    token: Annotated[str | None, Query()] = None,
+):
+    if session is None and token is None:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
+
+
+@app.websocket("/ws")
+async def infosocket(*,
+                     websocket: WebSocket,
+                     #item_id: str,
+                     #q: int | None = None,
+                     #cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
+                     ):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        #await websocket.send_text(f"Message text was: {data}")
+        mydata = {"name" : "POS1",
+                  "text" : "BE READY",
+                  "queue" : "empty",
+                  "lasttext" : data
+                  }
+        await websocket.send_json(mydata)
+
 
 class OpenPosBody(BaseModel):
     licenseToken: str = "",
@@ -105,16 +135,16 @@ def getevent(token: Annotated[str, Depends(oauth2_scheme)]) -> dict:
 def executeposcmd(token: Annotated[str, Depends(oauth2_scheme)], body: CommandBody) -> dict:
     print("token ", token)
     print("BODY ", body)
-    commandName = body.header.command
+    commandname = body.header.command
     params = body.params
-    print(f"commandName={commandName}")
+    print(f"commandName={commandname}")
 #   print(f"params={params}")
     result = {
         "resultCode": "OK",
-        "resultMessage": f"{commandName} was sent successfull!",
+        "resultMessage": f"{commandname} was sent successfull!",
         "resultTime": ""
     }
-    match commandName:
+    match commandname:
         case "UNLOCKDEVICE":
 
             Events.createevent_ONCARD(body)
@@ -137,7 +167,6 @@ def executeposcmd(token: Annotated[str, Depends(oauth2_scheme)], body: CommandBo
             print(body)
 
     return result
-
 
 
 def checkAuth(authorization: str):
